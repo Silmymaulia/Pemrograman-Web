543cbddfef4dd90e69e0c8ee79348386aef33194
Silmymaulia <silmy.smd@gmail.com>
2024-04-13 21:19:22 +0700

    Pertemuan12-OOP

---
+++ jobsheet12/oop.php
@@ -0,0 +1,81 @@
+<?php
+    class Car
+    {
+        public $brand;
+
+        public function startEngine()
+        {
+            echo "Engine started!";
+        }
+    }
+
+    $car1 = new Car();
+    $car1->brand = "Toyota";
+
+    $car2 = new Car();
+    $car2->brand = "Honda";
+
+    $car1->startEngine();
+    echo $car2->brand;
+
+    //langkah7
+    class Car
+{
+    private $model;
+    private $color;
+
+    public function __construct($model, $color)
+    {
+        $this->model = $model;
+        $this->color = $color;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+}
+
+$car = new Car("Toyota", "Blue");
+echo "Model: " . $car->getModel() . "<br>";
+echo "Color: " . $car->getColor() . "<br>";
+
+$car->setColor("Red");
+echo "Updated Color: " . $car->getColor() . "<br>";
+
+//langkah 10
+class Car
+{
+    private $brand;
+
+    public function __construct($brand)
+    {
+        echo "A new car is created.<br>";
+        $this->brand = $brand;
+    }
+
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    public function __destruct()
+    {
+        echo "The car is destroyed.<br>";
+    }
+}
+
+$car = new Car("Toyota");
+echo "Brand: " . $car->getBrand() . "<br>";
+
+?>
